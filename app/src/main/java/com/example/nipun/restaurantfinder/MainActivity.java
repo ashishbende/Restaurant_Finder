fb9c435fc7bbd489452154fca25ee1923dc8add4
Ashish Bende <eashishbende@gmail.com>
2016-03-18 17:52:21 -0700

    House keeping

--- app/src/main/java/com/example/nipun/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/example/nipun/restaurantfinder/MainActivity.java
@@ -65,23 +65,9 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-
-
-
-
-
-
         mName = (TextView) findViewById(R.id.textView);
         mAddress = (TextView) findViewById(R.id.textView2);
         mAttributions = (TextView) findViewById(R.id.textView3);
-
-
-
-
-
-
-
-
 
 
 
@@ -267,17 +253,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_search) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_favorite) {
 
         }
 
